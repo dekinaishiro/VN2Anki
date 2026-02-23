e445f368ad06f7d7b7ad964f3cd1f0142065a72f
dekinaishiro <dekinyaishiro@gmail.com>
2026-02-23 13:41:51 -0300

    add a 'play audio' button for history window slots

--- VN2Anki/MiningWindow.xaml.cs
+++ VN2Anki/MiningWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using VN2Anki.Models;
+using System.IO;
+using NAudio.Wave;
 
 namespace VN2Anki
 {
@@ -10,7 +12,11 @@ namespace VN2Anki
     {
         private static MiningWindow _instance;
         private static Action<MiningSlot> _onMineAction;
-        private static Action<MiningSlot> _onDeleteAction; 
+        private static Action<MiningSlot> _onDeleteAction;
+
+        private WaveOutEvent _waveOut;
+        private WaveFileReader _waveReader;
+        private MemoryStream _audioStream;
 
         public static void ShowWindow(ObservableCollection<MiningSlot> history, Action<MiningSlot> onMineAction, Action<MiningSlot> onDeleteAction)
         {
@@ -50,8 +56,69 @@ namespace VN2Anki
 
         protected override void OnClosed(EventArgs e)
         {
+            StopAudio();
+
             _instance = null;
             base.OnClosed(e);
         }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button?.Tag is MiningSlot slot)
+            {
+                PlayAudio(slot.AudioBytes);
+            }
+        }
+
+        private void PlayAudio(byte[] audioBytes)
+        {
+            // Para qualquer áudio que já esteja tocando
+            StopAudio();
+
+            if (audioBytes == null || audioBytes.Length == 0) return;
+
+            try
+            {
+                _audioStream = new MemoryStream(audioBytes);
+                _waveReader = new WaveFileReader(_audioStream);
+                _waveOut = new WaveOutEvent();
+
+                _waveOut.Init(_waveReader);
+
+                // Garante a liberação de recursos assim que o áudio terminar naturalmente
+                _waveOut.PlaybackStopped += (s, e) => StopAudio();
+
+                _waveOut.Play();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Erro ao reproduzir áudio: {ex.Message}");
+                StopAudio();
+            }
+        }
+
+        private void StopAudio()
+        {
+            // A ordem importa: primeiro para a reprodução, depois libera os leitores
+            if (_waveOut != null)
+            {
+                _waveOut.Stop();
+                _waveOut.Dispose();
+                _waveOut = null;
+            }
+            if (_waveReader != null)
+            {
+                _waveReader.Dispose();
+                _waveReader = null;
+            }
+            if (_audioStream != null)
+            {
+                _audioStream.Dispose();
+                _audioStream = null;
+            }
+        }
+
+
     }
 }
