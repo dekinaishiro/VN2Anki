cfdfdf25182321085782ff26e84d6be364038f7e
dekinaishiro <dekinyaishiro@gmail.com>
2026-04-24 16:25:12 -0300

    fix vn identification to prioritize executable path over process name

--- VN2Anki/Views/Hub/LibraryView.xaml.cs
+++ VN2Anki/Views/Hub/LibraryView.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace VN2Anki.Views.Hub
 {
@@ -9,21 +7,6 @@ namespace VN2Anki.Views.Hub
         public LibraryView()
         {
             InitializeComponent();
-        }
-
-        private void BtnAddVn_Click(object sender, RoutedEventArgs e)
-        {
-            var addWindow = App.Current.Services.GetRequiredService<AddVnWindow>();
-            addWindow.Owner = Window.GetWindow(this);
-
-            var vm = addWindow.DataContext as ViewModels.Hub.AddVnViewModel;
-            vm.IsOpenedFromLibrary = true;
-
-            if (addWindow.ShowDialog() == true)
-            {
-                var libraryVm = this.DataContext as ViewModels.Hub.LibraryViewModel;
-                _ = libraryVm?.LoadLibraryAsync();
-            }
         }
     }
 }
